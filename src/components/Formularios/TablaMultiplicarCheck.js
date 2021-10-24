d5f07a418de7a05b9b620e69e447764a83f38dd7
attiaApp <attiapp123@gmail.com>
2021-10-24 22:42:11 +0200

    update

--- src/components/Formularios/TablaMultiplicarCheck.js
+++ src/components/Formularios/TablaMultiplicarCheck.js
@@ -3,21 +3,55 @@ import React, { Component } from 'react'
     export default class TablaMultiplicarCheck extends Component {
 
 
-        numero = React.createRef();
+      
 
         state = {
-            check: false
+            check: false,
+            tabla:[],
+            num: 0,
+            checksArr:[]
         }
 
+        //Funcion que en principio generaba los checksbox pero ahora genera solo los numeros
+        generarCheck = (e) =>{
+            e.preventDefault();
+            var checks = []
 
-        cambio = (e) =>{
+            for(var i =0; i<=5; i++){
+                var aleatorio = Math.round((Math.random()*50)+1);
+               // checks.push(<div key={i}><label>{aleatorio}</label><input type='checkbox' onChange={this.cambio} value={aleatorio}></input></div>);
+                checks.push(aleatorio);
+            }
+
+          this.setState({
+              checksArr:checks
+          });
+            
+            //return checks
+        }
+
+        //Funcion que determina si el checkbox esta marcado o no y se crea la tabla de multiplicar
+        cambio = () =>{
             this.setState({
                 check: e.target.checked
             });
-            console.log(this.state.check);
+            
+            var array = []; // Array para guardar los resultados de las multiplicaciones
 
             if(this.state.check==false){
-                console.log(e.target.value);
+              var numero = e.target.value;
+              for( var i = 0; i <= 10; i++){
+                var multi = numero * i;
+                array.push(multi);
+                
+              } 
+
+              //Guardamos en la variable de estado tabla el array con los resultados y el numero que hemos 
+              //seleccionado, el numero es necesario para mostrar el resultado de 5*1 5*2... el numero 5 se repite por eso es necesario
+              this.setState({
+                tabla:array,
+                num: numero
+              });
             }
         }
 
@@ -27,14 +61,32 @@ import React, { Component } from 'react'
 
         render() {
             return (
-                <div style={{display:'inline-block'}}>
-                   
-                 <label>4</label>
-                 <input type='checkbox' onChange={this.cambio} value='4'/>
-                 <label>5</label>
-                 <input type='checkbox' onChange={this.cambio} value='5'/>
-                   
-            
+                <div >
+                    <form onSubmit={this.generarCheck}>
+                <button >Generar numeros</button>
+                <div>{this.state.checksArr.map((num, index) => {
+                   return(<div><label key={index}>{num}</label><input type='checkbox' value={num} onChange={this.cambio}></input></div>)
+                })}</div>
+                </form>
+                <div>
+                    <table border='1' style={{margin:'auto'}}>
+                    <thead>
+                        <tr>
+                            <th>
+                                Operación
+                            </th>
+                            <th>
+                                Resultado
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                       {this.state.tabla.map((num, index) =>{
+                           return(<tr key={index}><td>{this.state.num} * {index}</td><td>{num}</td></tr>)
+                       })}
+                    </tbody>
+                    </table>
+                </div>
 
                   
                 </div>
